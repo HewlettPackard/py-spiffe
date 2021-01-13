5c1268eb21eee6a29431a522bcf053aad9160be6
Maximiliano Churichi <maximiliano.churichi@hpe.com>
2021-01-13 15:33:49 -0300

    Adds SPIFFE endpoint socket validations
    Signed-off-by: Maximiliano Churichi <maximiliano.churichi@hpe.com>

--- src/pyspiffe/config.py
+++ src/pyspiffe/config.py
@@ -1,5 +1,8 @@
 import os
-from typing import Dict
+import ipaddress
+from urllib.parse import ParseResult, urlparse
+from typing import Dict, List, Tuple
+
 
 SPIFFE_ENDPOINT_SOCKET = 'SPIFFE_ENDPOINT_SOCKET'
 
@@ -18,4 +21,73 @@ class ConfigSetter:
         }
 
     def _apply_environment_variables(self):
-        self._config[SPIFFE_ENDPOINT_SOCKET] = os.environ.get(SPIFFE_ENDPOINT_SOCKET)
+        endpoint_socket = os.environ.get(SPIFFE_ENDPOINT_SOCKET)
+
+        if endpoint_socket is not None:
+            SpiffeEndpointSocket.validate(endpoint_socket)
+            self._config[SPIFFE_ENDPOINT_SOCKET] = endpoint_socket
+
+
+ENDPOINT_VALID_SCHEMES = ('unix', 'tcp')
+
+
+class SpiffeEndpointSocket:
+    @staticmethod
+    def validate(socket: str):
+        parsed_socket = urlparse(socket)
+
+        if parsed_socket.scheme == '':
+            raise ValueError('SPIFFE endpoint socket: scheme must be set')
+
+        if parsed_socket.scheme == 'unix':
+            SpiffeEndpointSocket.__validate_unix_socket(parsed_socket)
+        elif parsed_socket.scheme == 'tcp':
+            SpiffeEndpointSocket.__validate_tcp_socket(parsed_socket)
+        else:
+            raise ValueError('SPIFFE endpoint socket: unsupported scheme')
+
+    @staticmethod
+    def __validate_unix_socket(socket: ParseResult):
+        if socket.path is None:
+            raise ValueError('SPIFFE endpoint socket: path must be set')
+
+        INVALID_COMPONENTS = [
+            ('fragment', None),
+            ('username', None),
+            ('password', None),
+            ('query', None),
+            ('netloc', 'authority'),
+        ]
+        SpiffeEndpointSocket.__validate_forbidden_components(socket, INVALID_COMPONENTS)
+
+    @staticmethod
+    def __validate_tcp_socket(socket: ParseResult):
+        try:
+            ipaddress.ip_address(socket.hostname)
+        except:
+            raise ValueError('SPIFFE endpoint socket: host must be an IP address')
+
+        INVALID_COMPONENTS = [
+            ('path', None),
+            ('fragment', None),
+            ('username', None),
+            ('password', None),
+            ('query', None),
+        ]
+        SpiffeEndpointSocket.__validate_forbidden_components(socket, INVALID_COMPONENTS)
+
+    @staticmethod
+    def __validate_forbidden_components(
+        socket: ParseResult, components: List[Tuple[str, str]]
+    ):
+        for component, description in components:
+            try:
+                attr = getattr(socket, component)
+                if attr is not None and attr != '':
+                    raise ValueError(
+                        'SPIFFE endpoint socket: {} is not allowed'.format(
+                            description or component
+                        )
+                    )
+            except AttributeError:
+                pass
--- test/test_config.py
+++ test/test_config.py
@@ -1,4 +1,5 @@
-from src.pyspiffe.config import ConfigSetter
+import pytest
+from pyspiffe.config import ConfigSetter
 import os
 
 
@@ -22,3 +23,95 @@ def test_read_value_from_environment_variables():
     del os.environ['SPIFFE_ENDPOINT_SOCKET']
 
     assert setter.get_config()['SPIFFE_ENDPOINT_SOCKET'] == fake_socket
+
+
+# Path Validation tests
+def test_path_scheme_is_valid_unix():
+    fake_socket = 'unix:///path/to/endpoint.sock'
+    os.environ['SPIFFE_ENDPOINT_SOCKET'] = fake_socket
+
+    setter = ConfigSetter()
+
+    assert setter.get_config()['SPIFFE_ENDPOINT_SOCKET'] == fake_socket
+
+
+def test_path_scheme_is_valid_tcp():
+    fake_socket = 'tcp://127.0.0.1:8000'
+    os.environ['SPIFFE_ENDPOINT_SOCKET'] = fake_socket
+
+    setter = ConfigSetter()
+
+    assert setter.get_config()['SPIFFE_ENDPOINT_SOCKET'] == fake_socket
+
+
+@pytest.mark.parametrize(
+    'test_input,expected',
+    [
+        (
+            'invalid-socket',
+            'SPIFFE endpoint socket: scheme must be set',
+        ),
+        (
+            'http://example.org',
+            'SPIFFE endpoint socket: unsupported scheme',
+        ),
+        (
+            'spiffe://example.org',
+            'SPIFFE endpoint socket: unsupported scheme',
+        ),
+        (
+            'unix://authority/path/to/socket',
+            'SPIFFE endpoint socket: authority is not allowed',
+        ),
+        (
+            'unix://authority:8000/path/to/socket',
+            'SPIFFE endpoint socket: authority is not allowed',
+        ),
+        (
+            'unix:///path/to/socket?query=true',
+            'SPIFFE endpoint socket: query is not allowed',
+        ),
+        (
+            'unix:///path/to/socket?#fragment',
+            'SPIFFE endpoint socket: fragment is not allowed',
+        ),
+        (
+            'unix://user:@/path/to/socket',
+            'SPIFFE endpoint socket: username is not allowed',
+        ),
+        (
+            'unix://:pass@/path/to/socket',
+            'SPIFFE endpoint socket: password is not allowed',
+        ),
+        (
+            'tcp://localhost:8000',
+            'SPIFFE endpoint socket: host must be an IP address',
+        ),
+        (
+            'tcp://127.0.0.1:8000/path',
+            'SPIFFE endpoint socket: path is not allowed',
+        ),
+        (
+            'tcp://127.0.0.1?query=true',
+            'SPIFFE endpoint socket: query is not allowed',
+        ),
+        (
+            'tcp://127.0.0.1?#fragment',
+            'SPIFFE endpoint socket: fragment is not allowed',
+        ),
+        (
+            'tcp://user:@192.168.0.100',
+            'SPIFFE endpoint socket: username is not allowed',
+        ),
+        (
+            'tcp://:pass@192.168.0.100',
+            'SPIFFE endpoint socket: password is not allowed',
+        ),
+    ],
+)
+def test_invalid_endpoint_socket(test_input, expected):
+    with pytest.raises(ValueError) as exception:
+        os.environ['SPIFFE_ENDPOINT_SOCKET'] = test_input
+        ConfigSetter()
+
+    assert str(exception.value) == expected
