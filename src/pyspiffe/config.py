32b810101b6067d415acf80b1b56724928a4caf0
Maximiliano Churichi <maximiliano.churichi@hpe.com>
2021-01-19 17:24:24 -0300

    Addresses PR comments
    Signed-off-by: Maximiliano Churichi <maximiliano.churichi@hpe.com>

--- src/pyspiffe/config.py
+++ src/pyspiffe/config.py
@@ -8,6 +8,26 @@ SPIFFE_ENDPOINT_SOCKET = 'SPIFFE_ENDPOINT_SOCKET'
 
 
 class ConfigSetter:
+    """
+    Loads and validates configuration variables from the environment
+
+    Raises:
+        ValueError: if any variable from the environment has an invalid format
+    """
+
+    FORBIDDEN_SOCKET_COMPONENTS = [
+        ('fragment', None),
+        ('username', None),
+        ('password', None),
+        ('query', None),
+    ]
+
+    UNIX_FORBIDDEN_SOCKET_COMPONENTS = FORBIDDEN_SOCKET_COMPONENTS + [
+        ('netloc', 'authority')
+    ]
+
+    TCP_FORBIDDEN_SOCKET_COMPONENTS = FORBIDDEN_SOCKET_COMPONENTS + [('path', None)]
+
     def __init__(self):
         self._apply_default_config()
         self._apply_environment_variables()
@@ -15,74 +35,57 @@ class ConfigSetter:
     def get_config(self) -> Dict:
         return self._config
 
-    def _apply_default_config(self):
+    def _apply_default_config(self) -> None:
         self._config = {
             SPIFFE_ENDPOINT_SOCKET: None,
         }
 
-    def _apply_environment_variables(self):
+    def _apply_environment_variables(self) -> None:
         endpoint_socket = os.environ.get(SPIFFE_ENDPOINT_SOCKET)
 
-        if endpoint_socket is not None:
-            SpiffeEndpointSocket.validate(endpoint_socket)
+        if endpoint_socket:
+            self._validate(endpoint_socket)
             self._config[SPIFFE_ENDPOINT_SOCKET] = endpoint_socket
 
-
-class SpiffeEndpointSocket:
-    @staticmethod
-    def validate(socket: str):
+    def _validate(self, socket: str) -> None:
         parsed_socket = urlparse(socket)
 
-        if parsed_socket.scheme == '':
-            raise ValueError('SPIFFE endpoint socket: scheme must be set')
+        if not parsed_socket.scheme:
+            raise ValueError('SPIFFE endpoint socket: scheme must be set.')
 
         if parsed_socket.scheme == 'unix':
-            SpiffeEndpointSocket.__validate_unix_socket(parsed_socket)
+            self._validate_unix_socket(parsed_socket)
         elif parsed_socket.scheme == 'tcp':
-            SpiffeEndpointSocket.__validate_tcp_socket(parsed_socket)
+            self._validate_tcp_socket(parsed_socket)
         else:
-            raise ValueError('SPIFFE endpoint socket: unsupported scheme')
+            raise ValueError('SPIFFE endpoint socket: unsupported scheme.')
 
-    @staticmethod
-    def __validate_unix_socket(socket: ParseResult):
-        if socket.path is None:
-            raise ValueError('SPIFFE endpoint socket: path must be set')
+    @classmethod
+    def _validate_unix_socket(cls, socket: ParseResult) -> None:
+        if not socket.path:
+            raise ValueError('SPIFFE endpoint socket: path must be set.')
 
-        INVALID_COMPONENTS = [
-            ('fragment', None),
-            ('username', None),
-            ('password', None),
-            ('query', None),
-            ('netloc', 'authority'),
-        ]
-        SpiffeEndpointSocket.__validate_forbidden_components(socket, INVALID_COMPONENTS)
+        cls._validate_forbidden_components(socket, cls.UNIX_FORBIDDEN_SOCKET_COMPONENTS)
 
-    @staticmethod
-    def __validate_tcp_socket(socket: ParseResult):
+    @classmethod
+    def _validate_tcp_socket(cls, socket: ParseResult) -> None:
         try:
             ipaddress.ip_address(socket.hostname)
         except:
-            raise ValueError('SPIFFE endpoint socket: host must be an IP address')
+            raise ValueError('SPIFFE endpoint socket: host must be an IP address.')
 
-        INVALID_COMPONENTS = [
-            ('path', None),
-            ('fragment', None),
-            ('username', None),
-            ('password', None),
-            ('query', None),
-        ]
-        SpiffeEndpointSocket.__validate_forbidden_components(socket, INVALID_COMPONENTS)
+        cls._validate_forbidden_components(socket, cls.TCP_FORBIDDEN_SOCKET_COMPONENTS)
 
-    @staticmethod
-    def __validate_forbidden_components(
-        socket: ParseResult, components: List[Tuple[str, str]]
-    ):
+    @classmethod
+    def _validate_forbidden_components(
+        cls, socket: ParseResult, components: List[Tuple[str, str]]
+    ) -> None:
         for component, description in components:
             try:
                 attr = getattr(socket, component)
                 if attr is not None and attr != '':
                     raise ValueError(
-                        'SPIFFE endpoint socket: {} is not allowed'.format(
+                        'SPIFFE endpoint socket: {} is not allowed.'.format(
                             description or component
                         )
                     )
