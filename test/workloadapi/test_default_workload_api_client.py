4cf534e9329217cacea0ba2929b55cac4c7a17af
Scott Emmons <scotte@hpe.com>
2021-01-21 09:32:20 -0700

    SQUASH - fix what black formatted wrong!

--- test/workloadapi/test_default_workload_api_client.py
+++ test/workloadapi/test_default_workload_api_client.py
@@ -5,9 +5,8 @@ from pyspiffe.workloadapi.default_workload_api_client import DefaultWorkloadApiC
 
 # TODO: How to test functions that talk to workload API
 
+
 # No SPIFFE_ENDPOINT_SOCKET, and no path passed, raises exception
-
-
 def test_instantiate_default_without_var():
     del os.environ['SPIFFE_ENDPOINT_SOCKET']
     with pytest.raises(RuntimeError):
@@ -15,8 +14,6 @@ def test_instantiate_default_without_var():
 
 
 # With SPIFFE_ENDPOINT_SOCKET, and no path passed, succeeds
-
-
 def test_instantiate_default_with_var():
     os.environ['SPIFFE_ENDPOINT_SOCKET'] = '/tmp/agent.sock'
     wlapi = DefaultWorkloadApiClient()
@@ -25,32 +22,24 @@ def test_instantiate_default_with_var():
 
 
 # Pass socket path
-
-
 def test_instantiate_socket_path():
     wlapi = DefaultWorkloadApiClient(spiffe_socket_path='/tmp/agent.sock')
     assert wlapi.spiffe_socket_path == '/tmp/agent.sock'
 
 
 # TODO: Implement
-
-
 def test_fetch_x509_svid():
     wlapi = get_client()
     wlapi.fetch_x509_svid()
 
 
 # TODO: Implement
-
-
 def test_fetch_jx509_bundles():
     wlapi = get_client()
     wlapi.fetch_x509_bundles()
 
 
 # TODO: Implement
-
-
 def test_fetch_jwt_svid_aud():
     wlapi = get_client()
     audiences = ['foo', 'bar']
@@ -58,8 +47,6 @@ def test_fetch_jwt_svid_aud():
 
 
 # TODO: Implement
-
-
 def test_fetch_jwt_svid_aud_sub():
     wlapi = get_client()
     audiences = ['foo', 'bar']
@@ -67,16 +54,12 @@ def test_fetch_jwt_svid_aud_sub():
 
 
 # TODO: Implement
-
-
 def test_fetch_jwt_bundles():
     wlapi = get_client()
     wlapi.fetch_jwt_bundles()
 
 
 # TODO: Implement
-
-
 def test_validate_jwt_svid():
     wlapi = get_client()
     token = 'TODO'
@@ -85,7 +68,5 @@ def test_validate_jwt_svid():
 
 
 # Utility functions
-
-
 def get_client():
     return DefaultWorkloadApiClient(spiffe_socket_path='/tmp/agent.sock')
