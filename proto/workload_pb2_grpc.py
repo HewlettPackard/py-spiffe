be53df5153253c5abbed49985eb87d84faf744f3
Max Lambrecht <maxlambrecht@gmail.com>
2021-01-25 12:28:24 -0300

    Regenerating protobuf python code.
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- proto/workload_pb2_grpc.py
+++ proto/workload_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-from . import workload_pb2 as workload__pb2
+import workload_pb2 as workload__pb2
 
 
 class SpiffeWorkloadAPIStub(object):
