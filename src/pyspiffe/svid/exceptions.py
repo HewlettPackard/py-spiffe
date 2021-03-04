77537c63a98ac4d8b2d8f42c735b3b351ab6d51b
Max Lambrecht <max.lambrecht@hpe.com>
2021-03-04 09:40:33 -0300

    Implementation of X.509 SVID type. (#41)
    Implementation of X.509 SVID type.
    
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- src/pyspiffe/svid/exceptions.py
+++ src/pyspiffe/svid/exceptions.py
@@ -9,17 +9,13 @@ INVALID_VALUE_ERROR = '{} is not supported.'
 
 
 class JwtSvidError(PySpiffeError):
-    """Top level exception for JwtSvid module.
-
-    Attributes:
-        message: message describing the error.
-    """
+    """Top level exception for JwtSvid module. """
 
     def __init__(self, message: str) -> None:
         """Creates an instance of JwtSvidError.
 
         Args:
-            message: message describing the error.
+            message: Message describing the error.
         """
 
         self.message = message
@@ -32,10 +28,10 @@ class InvalidTokenError(JwtSvidError):
     """Error raised when provided token is invalid."""
 
     def __init__(self, message: str = '') -> None:
-        """Creates an instace of InvalidTokenError adding the provided message to the error mesage.
+        """Creates an instance of InvalidTokenError adding the provided message to the error message.
 
         Args:
-            message: contains additional information about the error.
+            message: Contains additional information about the error.
         """
         super().__init__(message)
 
@@ -45,11 +41,11 @@ class InvalidClaimError(JwtSvidError):
 
     _MESSAGE = 'Invalid claim value: {}.'
 
-    def __init__(self, additional_information: str = '') -> None:
-        """Creates an instace of InvalidClaimError adding the provided additional_information to the error mesage.
+    def __init__(self, additional_information: str) -> None:
+        """Creates an instace of InvalidClaimError adding the provided additional_information to the error message.
 
         Args:
-            additional_information: contains additional information about the error.
+            additional_information: Contains additional information about the error.
         """
         super().__init__(self._MESSAGE.format(additional_information))
 
@@ -69,11 +65,11 @@ class InvalidAlgorithmError(JwtSvidError):
 
     _MESSAGE = INVALID_VALUE_ERROR
 
-    def __init__(self, additional_information: str = '') -> None:
+    def __init__(self, additional_information: str) -> None:
         """Creates an instance of InvalidAlgorithmError
 
         Args:
-            additional_information: additional information about the error.
+            additional_information: Additional information about the error.
         """
         super().__init__(self._MESSAGE.format(additional_information))
 
@@ -83,10 +79,145 @@ class InvalidTypeError(JwtSvidError):
 
     _MESSAGE = INVALID_VALUE_ERROR
 
-    def __init__(self, additional_information: str = '') -> None:
+    def __init__(self, additional_information: str) -> None:
         """Creates an instance of InvalidTypeError
 
         Args:
-            additional_information: additional information about the error.
+            additional_information: Additional information about the error.
         """
         super().__init__(self._MESSAGE.format(additional_information))
+
+
+class X509SvidError(PySpiffeError):
+    """Top level exception for the X509Svid module. """
+
+    def __init__(self, message: str) -> None:
+        """Creates an instance of X509SvidError.
+
+        Args:
+            message: Message describing the error.
+        """
+
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class InvalidLeafCertificateError(X509SvidError):
+    """Error raised when an invalid leaf certificate is found in the X.509 chain."""
+
+    _MESSAGE = 'Invalid leaf certificate: {}.'
+
+    def __init__(self, additional_information: str) -> None:
+        """Creates an instance of InvalidLeafCertificateError
+
+        Args:
+            additional_information: Additional information about the error.
+        """
+        super().__init__(self._MESSAGE.format(additional_information))
+
+
+class InvalidIntermediateCertificateError(X509SvidError):
+    """Error raised when an invalid intermediate certificate is found in the X.509 chain."""
+
+    _MESSAGE = 'Invalid intermediate certificate: {}.'
+
+    def __init__(self, additional_information: str) -> None:
+        """Creates an instance of InvalidIntermediateCertificateError
+
+        Args:
+            additional_information: Additional information about the error.
+        """
+        super().__init__(self._MESSAGE.format(additional_information))
+
+
+class ParseCertificateError(X509SvidError):
+    """Error raised when an certificate could not be parsed from bytes."""
+
+    _MESSAGE = 'Error parsing certificate: {}.'
+
+    def __init__(self, additional_information: str) -> None:
+        """Creates an instance of ParseCertificateError
+
+        Args:
+            additional_information: Additional information about the error.
+        """
+        super().__init__(self._MESSAGE.format(additional_information))
+
+
+class ParsePrivateKeyError(X509SvidError):
+    """Error raised when the private key could not be parsed from bytes."""
+
+    _MESSAGE = 'Error parsing private key: {}.'
+
+    def __init__(self, additional_information: str) -> None:
+        """Creates an instance of ParsePrivateKeyError
+
+        Args:
+            additional_information: Additional information about the error.
+        """
+        super().__init__(self._MESSAGE.format(additional_information))
+
+
+class StoreCertificateError(X509SvidError):
+    """Error raised when an certificate could not be saved to disk."""
+
+    _MESSAGE = 'Error saving certificate to file: {}.'
+
+    def __init__(self, additional_information: str) -> None:
+        """Creates an instance of StoreCertificateError
+
+        Args:
+            additional_information: Additional information about the error.
+        """
+        super().__init__(self._MESSAGE.format(additional_information))
+
+
+class StorePrivateKeyError(X509SvidError):
+    """Error raised when the private key could not be saved to disk."""
+
+    _MESSAGE = 'Error saving private key to file: {}.'
+
+    def __init__(self, additional_information: str) -> None:
+        """Creates an instance of StorePrivateKeyError
+
+        Args:
+            additional_information: Additional information about the error.
+        """
+        super().__init__(self._MESSAGE.format(additional_information))
+
+
+class LoadCertificateError(X509SvidError):
+    """Error raised when an certificate could not be loaded from disk."""
+
+    _MESSAGE = 'Error loading certificate from file: {}.'
+
+    def __init__(self, additional_information: str) -> None:
+        """Creates an instance of LoadCertificateError
+
+        Args:
+            additional_information: Additional information about the error.
+        """
+        super().__init__(self._MESSAGE.format(additional_information))
+
+
+class LoadPrivateKeyError(X509SvidError):
+    """Error raised when the private key could not be loaded from disk."""
+
+    _MESSAGE = 'Error loading private key from file: {}.'
+
+    def __init__(self, additional_information: str) -> None:
+        """Creates an instance of LoadPrivateKeyError
+
+        Args:
+            additional_information: Additional information about the error.
+        """
+        super().__init__(self._MESSAGE.format(additional_information))
+
+
+def normalize_exception_message(msg: str) -> str:
+    """Removes the last point from the string."""
+    if msg[-1] != '.':
+        return msg
+    return msg[:-1]
--- src/pyspiffe/svid/x509_svid.py
+++ src/pyspiffe/svid/x509_svid.py
@@ -1,34 +1,473 @@
 """
-This module manages X509Svid objects.
+This module manages X.509 SVID objects.
 """
 
-from typing import Mapping, Set
-from datetime import datetime
+import os
+
+import pem
+from typing import List, Union
+from typing.io import BinaryIO
+
+from cryptography import x509
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ed25519, ed448, rsa, ec, dsa
+from cryptography.x509 import Certificate
+
 from pyspiffe.spiffe_id.spiffe_id import SpiffeId
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import load_der_private_key
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+
+from pyasn1.codec.der.decoder import decode
+
+from pyspiffe.svid.exceptions import (
+    InvalidLeafCertificateError,
+    InvalidIntermediateCertificateError,
+    ParseCertificateError,
+    ParsePrivateKeyError,
+    X509SvidError,
+    StoreCertificateError,
+    StorePrivateKeyError,
+    LoadCertificateError,
+    LoadPrivateKeyError,
+    normalize_exception_message,
+)
+
+_UNABLE_TO_LOAD_CERTIFICATE = 'Unable to load certificate'
+_CERTS_FILE_MODE = 0o644
+_PRIVATE_KEY_FILE_MODE = 0o600
+
+_PRIVATE_KEY_TYPES = Union[
+    ed25519.Ed25519PrivateKey,
+    ed448.Ed448PrivateKey,
+    rsa.RSAPrivateKey,
+    dsa.DSAPrivateKey,
+    ec.EllipticCurvePrivateKey,
+]
+
+__all__ = ['X509Svid']
 
 
 class X509Svid(object):
-    """Represents a SPIFFE X509 SVID."""
+    """
+    Represents a SPIFFE X.509-SVID.
+
+    Contains a SpiffeId, a private key and a chain of X.509 certificates.
+    """
 
     def __init__(
         self,
         spiffe_id: SpiffeId,
-        audience: Set[str],
-        expiry: datetime,
-        claims: Mapping[str, object],
-        token: str,
-    ):
-        """Creates a new X509Svid Object.
+        cert_chain: List[Certificate],
+        private_key: _PRIVATE_KEY_TYPES,
+    ) -> None:
+        """Creates a X509Svid instance.
 
         Args:
-            spiffe_id: SPIFFE ID of the JWT-SVID as present in the 'sub' claim.
-            audience: Audience is the intended recipients of JWT-SVID as present in the 'aud' claim.
-            expiry: Expiration time of JWT-SVID as present in 'exp' claim.
-            claims: Parsed claims from token.
-            token: Serialized JWT token.
+            spiffe_id: A SpiffeId instance.
+            cert_chain: A list representing a chain of X.509 Certificate.
+            private_key: A Private Key object.
         """
-        self.spiffe_id = spiffe_id
-        self.audience = audience
-        self.expiry = expiry
-        self.claims = claims
-        self.token = token
+
+        if spiffe_id is None:
+            raise ValueError('spiffe_id cannot be None.')
+
+        if not cert_chain:
+            raise ValueError('cert_chain cannot be empty.')
+
+        if private_key is None:
+            raise ValueError('private_key cannot be None.')
+
+        self._spiffe_id = spiffe_id
+        self._cert_chain = cert_chain
+        self._private_key = private_key
+
+    def leaf(self) -> Certificate:
+        """Returns the Leaf X.509 certificate of the chain."""
+        return self._cert_chain[0]
+
+    def cert_chain(self) -> List[Certificate]:
+        """Returns the X.509 chain of certificates."""
+        return self._cert_chain
+
+    def private_key(self) -> _PRIVATE_KEY_TYPES:
+        """Returns the private key."""
+        return self._private_key
+
+    def spiffe_id(self) -> SpiffeId:
+        """Returns the SpiffeId."""
+        return self._spiffe_id
+
+    @classmethod
+    def parse_raw(
+        cls, certs_chain_bytes: bytes, private_key_bytes: bytes
+    ) -> 'X509Svid':
+        """Parses the X509-SVID from certificate chain and private key bytes.
+
+        The certificate chain must be ASN.1 DER (concatenated with no intermediate padding if there are more than
+        one certificate). The private key must be a PKCS#8 ASN.1 DER.
+
+        It is assumed that the leaf certificate is always the first certificate in the parsed chain.
+
+        Args:
+            certs_chain_bytes: Chain of X.509 certificates in ASN.1 DER format.
+            private_key_bytes: Private key as PKCS#8 ASN.1 DER.
+
+        Returns:
+            An instance of a 'X509Svid' containing the chain of certificates, the private key, and the SPIFFE ID of the
+            leaf certificate in the chain.
+
+        Raises:
+            ParseCertificateError: In case the chain of certificates cannot be parsed from the cert_chain_bytes.
+            ParsePrivateKeyError: In case the private key cannot be parsed from the private_key_bytes.
+            InvalidLeafCertificateError: In case the leaf certificate does not have a SPIFFE ID in the URI SAN,
+                                         in case the leaf certificate is CA,
+                                         in case the leaf certificate has 'keyCertSign' as key usage,
+                                         in case the leaf certificate does not have 'digitalSignature' as key usage,
+                                         in case the leaf certificate does not have 'cRLSign' as key usage.
+            InvalidIntermediateCertificateError: In case one of the intermediate certificates is not CA,
+                                                 in case one of the intermediate certificates does not have 'keyCertSign' as key usage.
+        """
+
+        chain = _parse_der_certificates(certs_chain_bytes)
+        _validate_chain(chain)
+
+        private_key = _parse_der_private_key(private_key_bytes)
+        spiffe_id = _extract_spiffe_id(chain[0])
+
+        return X509Svid(spiffe_id, chain, private_key)
+
+    @classmethod
+    def parse(cls, certs_chain_bytes: bytes, private_key_bytes: bytes) -> 'X509Svid':
+        """Parses the X.509 SVID from PEM blocks containing certificate chain and key
+        bytes.
+
+        The private key must be a PKCS#8 PEM block.
+
+        It is assumed that the leaf certificate is always the first certificate in the parsed chain.
+
+        Args:
+            certs_chain_bytes: Chain of X.509 certificates in PEM format.
+            private_key_bytes: Private key as PKCS#8 PEM block.
+
+        Returns:
+            An instance of a 'X509Svid' containing the chain of certificates, the private key, and the SPIFFE ID of the
+            leaf certificate in the chain.
+
+        Raises:
+            ParseCertificateError: In case the chain of certificates cannot be parsed from the cert_chain_bytes.
+            ParsePrivateKeyError: In case the private key cannot be parsed from the private_key_bytes.
+            InvalidLeafCertificateError: In case the leaf certificate does not have a SPIFFE ID in the URI SAN,
+                                         in case the leaf certificate is CA,
+                                         in case the leaf certificate has 'keyCertSign' as key usage,
+                                         in case the leaf certificate does not have 'digitalSignature' as key usage,
+                                         in case the leaf certificate does not have 'cRLSign' as key usage.
+            InvalidIntermediateCertificateError: In case one of the intermediate certificates is not CA,
+                                                 in case one of the intermediate certificates does not have 'keyCertSign' as key usage.
+        """
+
+        chain = _parse_pem_certificates(certs_chain_bytes)
+
+        _validate_chain(chain)
+
+        private_key = _parse_pem_private_key(private_key_bytes)
+        spiffe_id = _extract_spiffe_id(chain[0])
+
+        return X509Svid(spiffe_id, chain, private_key)
+
+    @classmethod
+    def load(
+        cls,
+        certs_chain_path: str,
+        private_key_path: str,
+        encoding: serialization.Encoding,
+    ) -> 'X509Svid':
+        """Loads the X.509 SVID from PEM or DER encoded files on disk.
+
+        The private key should be without encryption.
+
+        Args:
+            certs_chain_path: Path to the file containing one or more X.509 certificates as PEM blocks.
+            private_key_path: Path the file containing a private key as PKCS#8 PEM block.
+            encoding: The encoding used to serialize the certs and private key, can be
+                                            serialization.Encoding.PEM or serialization.Encoding.DER.
+
+        Returns:
+            An instance of a 'X509Svid' containing the chain of certificates, the private key, and the SPIFFE ID of the
+            leaf certificate in the chain.
+
+        Raises:
+            ValueError: In case the encoding is not either PEM or DER (from serialization.Encoding).
+            X509SvidError: In case the file path in certs_chain_path or in private_key_path does not exists or cannot be open.
+            ParseCertificateError: In case the chain of certificates cannot be parsed from the bytes read from certs_chain_path.
+            ParsePrivateKeyError: In case the private key cannot be parsed from the bytes read from private_key_path.
+            InvalidLeafCertificateError: In case the leaf certificate does not have a SPIFFE ID in the URI SAN,
+                                         in case the leaf certificate is CA,
+                                         in case the leaf certificate has 'keyCertSign' as key usage,
+                                         in case the leaf certificate does not have 'digitalSignature' as key usage,
+                                         in case the leaf certificate does not have 'cRLSign' as key usage.
+            InvalidIntermediateCertificateError: In case one of the intermediate certificates is not CA,
+                                                 in case one of the intermediate certificates does not have 'keyCertSign' as key usage.
+        """
+
+        chain_bytes = _load_certs_bytes(certs_chain_path)
+        key_bytes = _load_private_key_bytes(private_key_path)
+
+        if encoding == serialization.Encoding.PEM:
+            return cls.parse(chain_bytes, key_bytes)
+
+        if encoding == serialization.Encoding.DER:
+            return cls.parse_raw(chain_bytes, key_bytes)
+
+        raise ValueError(
+            'Encoding not supported: {}. Expected \'PEM\' or \'DER\'.'.format(encoding)
+        )
+
+    @classmethod
+    def save(
+        cls,
+        x509_svid: 'X509Svid',
+        certs_chain_path: str,
+        private_key_path: str,
+        encoding: serialization.Encoding,
+    ) -> None:
+        """Saves the X.509 SVID certs chain and private key in PEM or DER encoded files on disk.
+
+        The private key is stored without encryption, but the file is set with filemode = '0600' (only owner has read/write permission).
+
+        Args:
+            x509_svid: the 'X509Svid' that has the certs_chain and private_key to be saved on disk.
+            certs_chain_path: Path to the file containing one or more X.509 certificates as PEM blocks.
+                                    The certs_chain file is configured with a filemode = '0644'.
+            private_key_path: Path the file containing a PKCS#8 PEM block.
+                                    The private_key file is configured with a filemode = '0600'.
+            encoding: The encoding used to serialize the certs and private key, can be
+                                            serialization.Encoding.PEM or serialization.Encoding.DER.
+
+        Raises:
+            ValueError: In case the encoding is not either PEM or DER (from serialization.Encoding).
+            X509SvidError: In case the certs chain or the private key in the X509Svid cannot be converted to bytes.
+            StorePrivateKeyError: In the case there is an error storing the private key to the file.
+            StoreCertificateError: In the case the file path in certs_chain_path cannot be open to write,
+                                  or there is an error storing the certificates to the file.
+        """
+
+        if encoding not in [encoding.PEM, encoding.DER]:
+            raise ValueError(
+                'Encoding not supported: {}. Expected \'PEM\' or \'DER\'.'.format(
+                    encoding
+                )
+            )
+
+        _write_certs_to_file(certs_chain_path, encoding, x509_svid)
+
+        private_key_bytes = _extract_private_key_bytes(
+            encoding, x509_svid.private_key()
+        )
+        _write_private_key_to_file(private_key_path, private_key_bytes)
+
+
+# Internal utility methods
+def _load_private_key_bytes(private_key_path: str) -> bytes:
+    try:
+        with open(private_key_path, 'rb') as key_file:
+            return key_file.read()
+    except FileNotFoundError:
+        raise LoadPrivateKeyError(
+            'Private key file not found: {}'.format(private_key_path)
+        )
+    except Exception as err:
+        raise LoadPrivateKeyError(
+            'Private key file could not be read: {}'.format(str(err))
+        )
+
+
+def _load_certs_bytes(certs_chain_path: str) -> bytes:
+    try:
+        with open(certs_chain_path, 'rb') as chain_file:
+            return chain_file.read()
+    except FileNotFoundError:
+        raise LoadCertificateError(
+            'Certs chain file file not found: {}'.format(certs_chain_path)
+        )
+    except Exception as err:
+        raise LoadCertificateError(
+            'Certs chain file could not be read: {}'.format(str(err))
+        )
+
+
+def _write_private_key_to_file(private_key_path: str, private_key_bytes: bytes) -> None:
+    try:
+        with open(private_key_path, 'wb') as private_key_file:
+            os.chmod(private_key_file.name, _PRIVATE_KEY_FILE_MODE)
+            private_key_file.write(private_key_bytes)
+    except Exception as err:
+        raise StorePrivateKeyError(
+            'Could not write private key bytes to file: {}'.format(str(err))
+        )
+
+
+def _extract_private_key_bytes(
+    encoding: serialization.Encoding, private_key: _PRIVATE_KEY_TYPES
+) -> bytes:
+    try:
+        return private_key.private_bytes(
+            encoding,
+            serialization.PrivateFormat.PKCS8,
+            serialization.NoEncryption(),
+        )
+    except Exception as err:
+        raise X509SvidError(
+            'Could extract private key bytes from object: {}'.format(
+                normalize_exception_message(str(err))
+            )
+        )
+
+
+def _write_certs_to_file(
+    certs_chain_path: str,
+    encoding: serialization.Encoding,
+    x509_svid: 'X509Svid',
+) -> None:
+    try:
+        with open(certs_chain_path, 'wb') as chain_file:
+            os.chmod(chain_file.name, _CERTS_FILE_MODE)
+            for cert in x509_svid.cert_chain():
+                _write_cert_to_file(cert, chain_file, encoding)
+    except Exception as err:
+        raise StoreCertificateError(
+            'Error opening certs chain file: {}'.format(str(err))
+        )
+
+
+def _write_cert_to_file(
+    cert: Certificate, chain_file: BinaryIO, encoding: serialization.Encoding
+) -> None:
+    try:
+        cert_bytes = _extract_cert_bytes(cert, encoding)
+        chain_file.write(cert_bytes)
+    except Exception as err:
+        raise StoreCertificateError(
+            'Error writing certs chain to file: {}'.format(str(err))
+        )
+
+
+def _extract_cert_bytes(cert: Certificate, encoding: serialization.Encoding) -> bytes:
+    try:
+        cert_bytes = cert.public_bytes(encoding)
+    except Exception as err:
+        raise X509SvidError(
+            'Could not get certs chain key bytes from object: {}'.format(str(err))
+        )
+
+    return cert_bytes
+
+
+def _parse_der_private_key(private_key_bytes: bytes) -> _PRIVATE_KEY_TYPES:
+    try:
+        return load_der_private_key(private_key_bytes, None, None)
+    except Exception as err:
+        raise ParsePrivateKeyError(normalize_exception_message(str(err)))
+
+
+def _parse_pem_private_key(private_key_bytes: bytes) -> _PRIVATE_KEY_TYPES:
+    try:
+        return load_pem_private_key(private_key_bytes, None, None)
+    except Exception as err:
+        raise ParsePrivateKeyError(normalize_exception_message(str(err)))
+
+
+def _parse_der_certificates(chain_der_bytes: bytes) -> List[Certificate]:
+    result = []
+    try:
+        leaf = x509.load_der_x509_certificate(chain_der_bytes, default_backend())
+        result.append(leaf)
+        _, remaining_data = decode(chain_der_bytes)
+        while len(remaining_data) > 0:
+            cert = x509.load_der_x509_certificate(remaining_data, default_backend())
+            result.append(cert)
+            _, remaining_data = decode(remaining_data)
+    except Exception as err:
+        raise ParseCertificateError(normalize_exception_message(str(err)))
+
+    if len(result) < 1:
+        raise ParseCertificateError(_UNABLE_TO_LOAD_CERTIFICATE)
+
+    return result
+
+
+def _parse_pem_certificates(chain_pem_bytes: bytes) -> List[Certificate]:
+    parsed_certs = pem.parse(chain_pem_bytes)
+    if not parsed_certs:
+        raise ParseCertificateError(_UNABLE_TO_LOAD_CERTIFICATE)
+
+    result = []
+    for cert in parsed_certs:
+        try:
+            x509_cert = x509.load_pem_x509_certificate(
+                cert.as_bytes(), default_backend()
+            )
+            result.append(x509_cert)
+        except Exception:
+            raise ParseCertificateError(_UNABLE_TO_LOAD_CERTIFICATE)
+
+    return result
+
+
+def _extract_spiffe_id(cert: Certificate) -> SpiffeId:
+    ext = cert.extensions.get_extension_for_oid(
+        x509.ExtensionOID.SUBJECT_ALTERNATIVE_NAME
+    )
+    sans = ext.value.get_values_for_type(x509.UniformResourceIdentifier)
+    if len(sans) == 0:
+        raise InvalidLeafCertificateError(
+            'Certificate does not contain a SPIFFE ID in the URI SAN'
+        )
+    return SpiffeId.parse(sans[0])
+
+
+def _validate_chain(cert_chain: List[Certificate]) -> None:
+    leaf = cert_chain[0]
+    _validate_leaf_certificate(leaf)
+
+    for cert in cert_chain[1:]:
+        _validate_intermediate_certificate(cert)
+
+
+def _validate_leaf_certificate(leaf: Certificate) -> None:
+    basic_constraints = leaf.extensions.get_extension_for_oid(
+        x509.ExtensionOID.BASIC_CONSTRAINTS
+    )
+    if basic_constraints.value.ca:
+        raise InvalidLeafCertificateError(
+            'Leaf certificate must not have CA flag set to true'
+        )
+    key_usage = leaf.extensions.get_extension_for_oid(x509.ExtensionOID.KEY_USAGE)
+    if not key_usage.value.digital_signature:
+        raise InvalidLeafCertificateError(
+            'Leaf certificate must have \'digitalSignature\' as key usage'
+        )
+    if key_usage.value.key_cert_sign:
+        raise InvalidLeafCertificateError(
+            'Leaf certificate must not have \'keyCertSign\' as key usage'
+        )
+    if key_usage.value.crl_sign:
+        raise InvalidLeafCertificateError(
+            'Leaf certificate must not have \'cRLSign\' as key usage'
+        )
+
+
+def _validate_intermediate_certificate(cert: Certificate) -> None:
+    basic_constraints = cert.extensions.get_extension_for_oid(
+        x509.ExtensionOID.BASIC_CONSTRAINTS
+    )
+    if not basic_constraints.value.ca:
+        raise InvalidIntermediateCertificateError(
+            'Signing certificate must have CA flag set to true'
+        )
+    key_usage = cert.extensions.get_extension_for_oid(x509.ExtensionOID.KEY_USAGE)
+    if not key_usage.value.key_cert_sign:
+        raise InvalidIntermediateCertificateError(
+            'Signing certificate must have \'keyCertSign\' as key usage'
+        )
