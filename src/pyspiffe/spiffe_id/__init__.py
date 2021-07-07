fbda87a25c5590b712dc1867a4240c22d40ff96d
Max Lambrecht <maxlambrecht@gmail.com>
2021-07-07 13:27:32 -0300

    Address PR comment.
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- src/pyspiffe/spiffe_id/__init__.py
+++ src/pyspiffe/spiffe_id/__init__.py
@@ -4,5 +4,5 @@ spiffe_id Module. Contains information related to SPIFFE Ids.
 
 SPIFFE_SCHEME = 'spiffe'
 """str: SPIFFE scheme."""
-SCHEME_PREFIX = 'spiffe://'
+SCHEME_PREFIX = SPIFFE_SCHEME + '://'
 """str: SPIFFE scheme prefix."""
